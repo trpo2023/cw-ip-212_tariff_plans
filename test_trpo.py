58503266e65893d054fcc7c6d9937704a1adbe33
d1kis555 <simskovdrago@gmail.com>
2023-05-24 00:15:10 +0700

    test trpo

--- test_trpo.py
+++ test_trpo.py
@@ -1,85 +1,64 @@
 import unittest
-from io import StringIO
-import sys
-import json
 from unittest.mock import patch
-from trpo import main_menu, choice_tariff, print_tariff_plans
+import io
+import json
+import sys
+from main import main_menu, choice_tariff, print_tariff_plans
 
-
-class TariffProgramTests(unittest.TestCase):
+class TariffMenuTestCase(unittest.TestCase):
     def setUp(self):
-        self.data = {
-            "tariff_plans": {
-                "Тариф 1": {
-                    "minutes": 250,
-                    "messages": 200,
-                    "internet": 1,
-                    "operator": "Оператор 1"
-                },
-                "Тариф 2": {
-                    "minutes": 300,
-                    "messages": 300,
-                    "internet": 3,
-                    "operator": "Оператор 2"
-                },
-                "Тариф 3": {
-                    "minutes": 600,
-                    "messages": 400,
-                    "internet": 5,
-                    "operator": "Оператор 3"
-                }
-            }
-        }
+        self.maxDiff = None
+        with open("tariff_plans.json") as file:
+            self.data = json.load(file)
 
-    def test_choice_tariff_valid_input(self):
-        with patch('builtins.input', side_effect=['1', '1', '1', 'да']):
-            with patch('sys.stdout', new=StringIO()) as fake_output:
-                with open("tariff_plans.json", "w") as file:
-                    json.dump(self.data, file)
-                choice_tariff()
-                expected_output = '''\
-======================
-Рекомендуемый тариф:
-Тариф: Тариф 1
-Оператор: Оператор 1
-Минуты: 250
-Сообщения: 200
-Интернет: 1 ГБ
-======================
-Вы хотите подключить данный тариф? (Да/Нет): 
-Тариф успешно подключен!
-'''
-                self.assertEqual(fake_output.getvalue(), expected_output)
+    @patch("builtins.input")
+    def test_main_menu_выбор_1(self, mock_input):
+        mock_input.side_effect = ["1", "2", "1", "4", "3"]  # Симулируем ввод пользователя
+        expected_output = (
+            "Выберите оператора:\n"
+            "1. MTS\n"
+            "2. Beeline\n"
+            "3. TELE2\n"
+            "4. Megafon\n"
+        )
+        with patch('sys.stdout', new=io.StringIO()) as fake_output:
+            main_menu()
+            self.assertEqual(fake_output.getvalue().strip(), expected_output)
 
-    def test_choice_tariff_invalid_input(self):
-        with patch('builtins.input', side_effect=['7', '10', '11', 'нет']):
-            with patch('sys.stdout', new=StringIO()) as fake_output:
-                with open("tariff_plans.json", "w") as file:
-                    json.dump(self.data, file)
-                choice_tariff()
-                expected_output = '''\
-Выберите количество минут разговора:
-Некорректный выбор. Введите номер выбранного варианта: 
-Некорректный выбор. Введите номер выбранного варианта: 
-Выберите количество интернет-трафика:
-Некорректный выбор. Введите номер выбранного варианта: 
-К сожалению, не удалось найти подходящий тариф.
-'''
-                self.assertEqual(fake_output.getvalue(), expected_output)
+    @patch("builtins.input")
+    def test_choice_tariff_подбор_тарифа(self, mock_input):
+        mock_input.side_effect = ["1", "2", "3", "4"]  # Симулируем ввод пользователя
+        expected_output = (
+            "======================\n"
+            "Рекомендуемый тариф:\n"
+            "Тариф: The_tariff_is_higher\n"
+            "Оператор: MTS\n"
+            "Минуты: 600\n"
+            "Сообщения: 600\n"
+            "Интернет: 35 ГБ\n"
+            "======================\n"
+            "Вы хотите подключить данный тариф? (Да/Нет): \n"
+            "Тариф успешно подключен!"
+        )
+        with patch('sys.stdout', new=io.StringIO()) as fake_output:
+            choice_tariff()
+            self.assertEqual(fake_output.getvalue().strip(), expected_output)
 
-    def test_print_tariff_plans(self):
-        with patch('sys.stdout', new=StringIO()) as fake_output:
-            with open("tariff_plans.json", "w") as file:
-                json.dump(self.data, file)
-            print_tariff_plans()
-            expected_output = '''\
-Доступные тарифы:
-======================
-Тариф: Тариф 1
-Минуты: 250
-Сообщения: 200
-Интернет: 1 ГБ
-Оператор: Оператор 1
+    @patch("sys.stdout", new_callable=io.StringIO)
+    def test_print_tariff_plans_просмотр_тарифов(self, mock_stdout):
+        expected_output = (
+            "Доступные тарифы:\n"
+            "======================\n"
+            "Тариф: MTS_access\n"
+            "Минуты: 5000\n"
+            "Сообщения: 1000\n"
+            "Интернет: 50 ГБ\n"
+            "Оператор: MTS\n"
+            "======================\n"
+            # Остальные тарифы...
+        )
+        print_tariff_plans()
+        self.assertEqual(mock_stdout.getvalue().strip(), expected_output)
 
-
-
+if __name__ == "__main__":
+    unittest.main()
