23de67417e05b4a59e271fce555f390deb264f8d
d1kis555 <simskovdrago@gmail.com>
2023-06-02 22:01:30 +0700

    update trpo.py

--- trpo.py
+++ trpo.py
@@ -6,20 +6,22 @@ def clear_console():
 
 def main_menu():
     while True:
-        
+        clear_console()
         print("======================")
         print("      ГЛАВНОЕ МЕНЮ     ")
         print("======================")
-        print("1. Подобрать тариф")
-        print("2. Просмотреть все тарифы")
-        print("3. Выйти из программы")
+        menu_options = ["Подобрать тариф", "Просмотреть все тарифы", "Выйти из программы"]
+        for i, option in enumerate(menu_options, start=1):
+            print(f"{i}. {option}")
         print("======================")
 
         choice = input("Выберите опцию (введите номер): ")
 
         if choice == "1":
+            clear_console()
             choice_tariff()
         elif choice == "2":
+            clear_console()
             print_tariff_plans()
         elif choice == "3":
             print("Выход из программы...")
@@ -28,120 +30,14 @@ def main_menu():
             print("Некорректный выбор. Попробуйте еще раз.")
 
 def choice_tariff():
-    def get_minutes():
-        print("Выберите количество минут разговора:")
-        print("1. 250 минут")
-        print("2. 300 минут")
-        print("3. 600 минут")
-        print("4. 1200 минут")
-        print("5. 3000 минут")
-        print("6. 5000 минут")
-        minutes_choice = input("Введите номер выбранного варианта: ")
-        while minutes_choice not in ["1", "2", "3", "4", "5", "6"]:
-            minutes_choice = input("Некорректный выбор. Введите номер выбранного варианта: ")
-
-        if minutes_choice == "1":
-            return 250
-        elif minutes_choice == "2":
-            return 300
-        elif minutes_choice == "3":
-            return 600
-        elif minutes_choice == "4":
-            return 1200
-        elif minutes_choice == "5":
-            return 3000
-        elif minutes_choice == "6":
-            return 5000
-
-    def choose_messages():
-        clear_console()
-        print("Выберите количество сообщений:")
-        print("1. 200 сообщений")
-        print("2. 300 сообщений")
-        print("3. 400 сообщений")
-        print("4. 500 сообщений")
-        print("5. 600 сообщений")
-        print("6. 700 сообщений")
-        print("7. 800 сообщений")
-        print("8. 1000 сообщений")
-        print("9. 3000 сообщений")
-        messages_choice = input("Введите номер выбранного варианта: ")
-        while messages_choice not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-            messages_choice = input("Некорректный выбор. Введите номер выбранного варианта: ")
-
-        if messages_choice == "1":
-            return 200
-        elif messages_choice == "2":
-            return 300
-        elif messages_choice == "3":
-            return 400
-        elif messages_choice == "4":
-            return 500
-        elif messages_choice == "5":
-            return 600
-        elif messages_choice == "6":
-            return 700
-        elif messages_choice == "7":
-            return 800
-        elif messages_choice == "8":
-            return 1000
-        elif messages_choice == "9":
-            return 3000
-
-    def choose_internet():
-        clear_console()
-        print("Выберите количество интернет-трафика:")
-        print("1. 1 ГБ")
-        print("2. 3 ГБ")
-        print("3. 5 ГБ")
-        print("4. 10 ГБ")
-        print("5. 15 ГБ")
-        print("6. 20 ГБ")
-        print("7. 30 ГБ")
-        print("8. 40 ГБ")
-        print("9. 50 ГБ")
-        internet_choice = input("Введите номер выбранного варианта: ")
-        while internet_choice not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
-            internet_choice = input("Некорректный выбор. Введите номер выбранного варианта: ")
-
-        if internet_choice == "1":
-            return 1
-        elif internet_choice == "2":
-            return 3
-        elif internet_choice == "3":
-            return 5
-        elif internet_choice == "4":
-            return 10
-        elif internet_choice == "5":
-            return 15
-        elif internet_choice == "6":
-            return 20
-        elif internet_choice == "7":
-            return 30
-        elif internet_choice == "8":
-            return 40
-        elif internet_choice == "9":
-            return 50
-
-    def choose_operator():
-        clear_console()
-        print("Выберите желаемого оператора:")
-        print("1. MTS")
-        print("2. Beeline")
-        print("3. TELE2")
-        print("4. Megafon")
-        operator_choice = input("Введите номер выбранного варианта: ")
-        while operator_choice not in ["1", "2", "3", "4"]:
-            operator_choice = input("Некорректный выбор. Введите номер выбранного варианта: ")
-
-        if operator_choice == "1":
-            return "MTS"
-        elif operator_choice == "2":
-            return "Beeline"
-        elif operator_choice == "3":
-            return "TELE2"
-        elif operator_choice == "4":
-            return "megafon"
+    def get_choice(prompt, options, user_choice):
+        print(prompt)
+        for i, option in enumerate(options, start=1):
+            print(f"{i}. {option}")
+        choice = user_choice
+        while choice not in map(str, range(1, len(options) + 1)):
+            choice = input("Некорректный выбор. Введите номер выбранного варианта: ")
+        return options[int(choice) - 1]
 
     with open("tariff_plans.json") as file:
         data = json.load(file)
@@ -149,10 +45,13 @@ def choice_tariff():
     tariff_plans = data["tariff_plans"]
 
     print("Укажите ваши затраты:")
-    minutes = get_minutes()
-    messages = choose_messages()
-    internet = choose_internet()
-    operator = choose_operator()
+    minutes = get_choice("Выберите количество минут разговора:", ["250 минут", "300 минут", "600 минут", "1200 минут", "3000 минут", "5000 минут"], "")
+    messages = get_choice("Выберите количество сообщений:", ["200 сообщений", "300 сообщений", "400 сообщений", "500 сообщений", "600 сообщений", "700 сообщений", "800 сообщений", "1000 сообщений", "3000 сообщений"], "")
+    internet = get_choice("Выберите количество интернет-трафика:", ["1 ГБ", "3 ГБ", "5 ГБ", "10 ГБ", "15 ГБ", "20 ГБ", "30 ГБ", "40 ГБ", "50 ГБ"], "")
+
+    clear_console()
+
+    operator = get_choice("Выберите желаемого оператора:", ["MTS", "Beeline", "TELE2", "Megafon"], "")
 
     optimal_tariff = None
     optimal_tariff_cost = float('inf')
@@ -164,38 +63,24 @@ def choice_tariff():
             tariff_internet = tariff_details["internet"]
             tariff_cost = tariff_minutes + tariff_messages + tariff_internet
 
-            if tariff_minutes >= minutes and tariff_messages >= messages and tariff_internet >= internet and tariff_cost < optimal_tariff_cost:
+            if tariff_minutes >= int(minutes.split()[0]) and tariff_messages >= int(messages.split()[0]) and tariff_internet >= int(internet.split()[0]) and tariff_cost < optimal_tariff_cost:
                 optimal_tariff = tariff_name
                 optimal_tariff_cost = tariff_cost
 
-    if optimal_tariff is not None:
-        optimal_tariff_operator = tariff_plans[optimal_tariff]["operator"]
+    if optimal_tariff:
+        minute_optimal = tariff_plans[optimal_tariff]['minutes']
+        message_optimal = tariff_plans[optimal_tariff]['messages']
+        internet_optimal = tariff_plans[optimal_tariff]['internet']
 
-        print("======================")
-        print("Рекомендуемый тариф:")
+        print("Наиболее оптимальный для вас тарифный план:")
         print(f"Тариф: {optimal_tariff}")
-        print(f"Оператор: {optimal_tariff_operator}")
-        print(f"Минуты: {tariff_plans[optimal_tariff]['minutes']}")
-        print(f"Сообщения: {tariff_plans[optimal_tariff]['messages']}")
-        print(f"Интернет: {tariff_plans[optimal_tariff]['internet']} ГБ")
-        print("======================")
-
-        confirmation = input("Вы хотите подключить данный тариф? (Да/Нет): ")
-        if confirmation.lower() == "да":
-            clear_console()
-            print(f"Тариф успешно подключен! Ежемесячная плата составит: {tariff_plans[optimal_tariff]['monthly_cost']} рублей.")
-            print("Выход в главное меню...")
-        else:
-            change_tariff = input("Хотите выбрать другой тариф? (Да/Нет): ")
-            if change_tariff.lower() == "да":
-                clear_console()
-                choice_tariff()
-            else:
-                clear_console()
-                print("Подключение тарифа отменено.")
-                print("Спасибо за использование нашего приложения!!!")
+        print(f"Минуты разговора: {minute_optimal}")
+        print(f"Количество сообщений: {message_optimal}")
+        print(f"Интернет-трафик: {internet_optimal}")
     else:
-        print("К сожалению, не удалось найти подходящий тариф.")
+        print("Извините, для ваших затрат не найдено подходящего тарифного плана.")
+
+    input("Нажмите Enter, чтобы вернуться в главное меню.")
 
 def print_tariff_plans():
     with open("tariff_plans.json") as file:
@@ -203,15 +88,15 @@ def print_tariff_plans():
 
     tariff_plans = data["tariff_plans"]
 
-    print("Доступные тарифы:")
     for tariff_name, tariff_details in tariff_plans.items():
-        print("======================")
         print(f"Тариф: {tariff_name}")
-        print(f"Минуты: {tariff_details['minutes']}")
-        print(f"Сообщения: {tariff_details['messages']}")
-        print(f"Интернет: {tariff_details['internet']} ГБ")
         print(f"Оператор: {tariff_details['operator']}")
+        print(f"Минуты разговора: {tariff_details['minutes']}")
+        print(f"Количество сообщений: {tariff_details['messages']}")
+        print(f"Интернет-трафик: {tariff_details['internet']}")
+        print("======================")
 
-# Запуск программы
-clear_console()
-main_menu()
+    input("Нажмите Enter, чтобы вернуться в главное меню.")
+
+if __name__ == "__main__":
+    main_menu()
